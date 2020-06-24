dafc98d708d9959d66e80664076434d172724c54
bhernandez-mycwi <55470962+bhernandez-mycwi@users.noreply.github.com>
2020-06-24 13:34:23 -0600

    Create addAnchors.js
    //The theme we use only allows us to assign a class rather than an ID. We target the class of the wrapper which holds an image and a text description and wrap it in an <a> tag

---
+++ addAnchors.js
@@ -0,0 +1,37 @@
+//The theme we use only allows us to assign a class rather than an ID. We target the class of the wrapper which holds an image and a text description and wrap it in an <a> tag
+
+//agriculture section 
+$(".agriculture").wrap("<a href='industrys-we-service/#agriculture'></a>");
+
+//water control section 
+$(".waterControl").wrap("<a href='industrys-we-service/#waterControl'></a>");
+
+//Material handling section 
+$(".material").wrap("<a href='industrys-we-service/#material'></a>");
+
+//Oil and Gas section 
+$(".oil").wrap("<a href='industrys-we-service/#oil'></a>");
+
+// plastics section 
+$(".plastics").wrap("<a href='industrys-we-service/#plastics'></a>");
+
+// food section 
+$(".food").wrap("<a href='industrys-we-service/#food'></a>");
+
+// Mills section 
+$(".mill").wrap("<a href='industrys-we-service/#mill'></a>");
+
+// packagin section 
+$(".packaging ").wrap("<a href='industrys-we-service/#packaging '></a>");
+
+// timber section 
+$(".timber ").wrap("<a href='industrys-we-service/#timber '></a>");
+
+// manufacturing section 
+$(".manufacturing").wrap("<a href='industrys-we-service/#manufacturing'></a>");
+
+// Mining section 
+$(".mining ").wrap("<a href='industrys-we-service/#mining '></a>");
+
+// power Generator section 
+$(".powerGen").wrap("<a href='industrys-we-service/#powerGen'></a>");
